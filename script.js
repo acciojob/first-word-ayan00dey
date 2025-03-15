4b758f80fee4dd6a2dabaa9b6927dc6a97955f26
ayan00dey <mr.dey.ayanava@gmail.com>
2025-03-15 23:10:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function firstWord(s) {
   // your code here
-	let s = s.trim();
-	int spaceIdx = s.indexOf(' ');
-	if(spaceIdx==-1) return -1;
-	return s.substring(0,spaceIdx);
+	 // Trim leading spaces
+  s = s.trim();
+  
+  // Find the index of the first space
+  let spaceIndex = s.indexOf(' ');
+  
+  // If no space is found, return the entire string
+  if (spaceIndex === -1) return s;
+  
+  // Return the substring from start to the first space
+  return s.substring(0, spaceIndex);
 	
 }
 
