3a3e680a2605925191d4607e677024e4bd5fb96e
ayan00dey <mr.dey.ayanava@gmail.com>
2025-03-15 23:06:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstWord(s) {
   // your code here
+	let s = s.trim();
+	int spaceIdx = s.indexOf(' ');
+	if(spaceIdx==-1) return -1;
+	return s.substring(0,spaceIdx);
+	
 }
 
 // Do not change the code below
